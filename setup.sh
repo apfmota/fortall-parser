57ea1ade4ca093b052d662a68e286b4ce45a35c0
AndrielPrieto <andriel@megatecnologia.com.br>
2025-07-01 12:21:49 -0300

    removing unecessary encoding flag

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 java -Dfile.encoding=UTF-8 -jar antlr-4.13.2-complete.jar -visitor -package compiladores.fortall.parser src/main/java/compiladores/fortall/parser/Fortall.g4 -o src/main/java/compiladores/fortall/parser/
 mvn clean package
 echo 'Setup finalizado'
-echo 'Para rodar o programa utilize: java -Dfile.encoding=UTF-8 -jar target/fortall-parser-1.0.jar <arquivo_input>'
+echo 'Para rodar o programa utilize: java -jar target/fortall-parser-1.0.jar <arquivo_input>'
